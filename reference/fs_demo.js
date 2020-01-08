3e71ca0aa29ae0d67504c0c3dd191736a1dc0654
jsagithub <jsa_epe@hotmail.com>
2020-01-08 06:18:39 +0000

    Add new text to a file and read the file content

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,11 +8,30 @@ const path = require("path");
 // });
 
 //Create and Write to File
-fs.writeFile(
-  path.join(__dirname, "/test", "hello.txt"),
-  "Hello World",
-  function(err) {
-    if (err) throw err;
-    console.log("File writen to..");
-  }
-);
+// fs.writeFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "Hello World",
+//   function(err) {
+//     if (err) throw err;
+//     console.log("File writen to..");
+//     //Add new text to file
+//     fs.appendFile(
+//       path.join(__dirname, "/test", "hello.txt"),
+//       " I love nodeJs",
+//       function(err) {
+//         if (err) throw err;
+//         console.log("File writen to..");
+//       }
+//     );
+//   }
+// );
+
+//Read File
+
+fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", function(
+  err,
+  data
+) {
+  if (err) throw err;
+  console.log(data);
+});
