1f7d6f04a42ed497645f2741351598897e6c8d7a
jsagithub <jsa_epe@hotmail.com>
2020-01-09 19:31:22 +0000

    change files name

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -28,10 +28,20 @@ const path = require("path");
 
 //Read File
 
-fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", function(
-  err,
-  data
-) {
-  if (err) throw err;
-  console.log(data);
-});
+// fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", function(
+//   err,
+//   data
+// ) {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+//Rename file
+fs.rename(
+  path.join(__dirname, "/test", "hello.txt"),
+  path.join(__dirname, "/test", "helloworld.txt"),
+  function(err) {
+    if (err) throw err;
+    console.log("File renamed");
+  }
+);
