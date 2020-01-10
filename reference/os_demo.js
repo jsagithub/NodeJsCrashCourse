e95ba76697a2e9d9d31167cadfb8a0efaecfaf29
jsagithub <jsa_epe@hotmail.com>
2020-01-10 19:05:44 +0000

    CPU and plataform info

--- reference/os_demo.js
+++ reference/os_demo.js
@@ -2,3 +2,12 @@ const os = require("os");
 
 // GEt Operative System
 console.log(os.platform());
+
+//CPU arquitecture
+console.log(os.arch());
+
+//CPU Core Info
+console.log(os.cpus());
+
+//Free memory
+console.log(os.freemem());
